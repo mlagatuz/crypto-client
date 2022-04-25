3ee7829378a62d4e697134d615726c6909a1aadb
Mark Lagatuz <mlagatuz@mbp.lan>
2022-04-25 00:07:43 -0700

    testing KuCoin access

--- test_my_class.py
+++ test_my_class.py
@@ -10,17 +10,32 @@ Created on Apr 21, 2022
 import json, os
 from crypto_client import CryptoClient
 
-EXCHANGE = 'CBP'
+EXCHANGE = 'KC'
 api_key = os.getenv(f'{EXCHANGE}_API_KEY')
 api_secret = os.getenv(f'{EXCHANGE}_API_SECRET')
-api_version = '2021-03-20' if EXCHANGE == 'CB' else None
-api_passphrase = os.getenv('CBP_API_PASSPHRASE') if EXCHANGE == 'CBP' else None
+api_dict = {
+            'CB':'2021-03-20',
+            'CBP':None,
+            'KC':'2'
+            }
+api_version = api_dict[EXCHANGE]
+api_passphrase = os.getenv(f'{EXCHANGE}_API_PASSPHRASE') if (EXCHANGE == 'CBP' or EXCHANGE == 'KC') else None
 
-base_url = 'https://api.exchange.coinbase.com' if EXCHANGE == 'CBP' else 'https://api.coinbase.com'
+url_dict = {
+            'CB':'https://api.coinbase.com',
+            'CBP':'https://api.exchange.coinbase.com',
+            'KC':'https://api.kucoin.com'
+            }
+base_url = url_dict[EXCHANGE]
+
 client = CryptoClient(api_key, api_secret, base_url, EXCHANGE, api_version, api_passphrase)
 
 account_id = os.getenv(f'{EXCHANGE}_BTC_ACCOUNT_ID')
-relative_path = '/accounts/' + account_id if EXCHANGE == 'CBP' else '/v2/accounts/' + account_id
+accounts_path_dict = {
+                    'CB':'/v2/accounts/',
+                    'CBP':'/accounts/',
+                    'KC':f'/api/v1/accounts'}
+relative_path = accounts_path_dict[EXCHANGE] + account_id
 body = ''
 method = 'GET'
 
